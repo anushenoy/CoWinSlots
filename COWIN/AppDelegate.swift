b0fb9446f5244b58bc91185a38c4da558ea41219
anush <anush_shenoy@yahoo.co.in>
2021-05-02 00:22:27 +0530

    Add background fetch

--- COWIN/AppDelegate.swift
+++ COWIN/AppDelegate.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    let notificationCenter = UNUserNotificationCenter.current()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        notificationCenter.requestAuthorization(options: options) {
+            (didAllow, error) in
+            if !didAllow {
+                print("User has declined notifications")
+            }
+        }
         return true
     }
 
@@ -30,7 +39,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        notificationCenter.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                // Notifications  allowed
+                if let pin = UserDefaults.standard.string(forKey: "pincode"){
+                    ViewModel().isAvailable(pincode: pin, onComplete: { flag in
+                        guard flag else {
+                            return
+                        }
+                        let content = UNMutableNotificationContent()
+                        content.title = "Cowin Slot Available"
+                        content.body = "App has detected that a slot has opened up"
+                        content.sound = UNNotificationSound.default
+                        let request = UNNotificationRequest(
+                            identifier: UUID().uuidString,
+                            content: content,
+                            trigger: UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+                        )
+                        UNUserNotificationCenter.current().add(request)
+                    })
+                }
+            }
+        }
+    }
 
 }
 
